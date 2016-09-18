28fd8421a39ff60b8231398d5516a7109e920105
ejarkm <ejarkm@gmail.com>
2016-09-18 18:54:20 +0200

    Class and functions created

--- functions.R
+++ functions.R
@@ -1,6 +1,32 @@
-linreg <- setRefClass("RCclass")
-linreg$methods(coefficients
-               = function(formula){
-                 
+linreg <- setRefClass("RCclass", fields = c("indep", "dep", "predicted", "residuals", "data", "formula"))
+linreg$methods(list(coefficients
+               = function(){
+                  indep <<- model.frame(formula)
+                  dep <<- as.matrix(indep[1])
+                  colnames(dep) <<- colnames(indep)[1]
+                  indep <<- cbind(rep(1,length(indep)),as.matrix(indep[-1]))
+                  solve(crossprod(indep))%*%t(indep)%*%dep
                }
-               )
+               ,pred
+               = function(){
+                 predicted <<- indep%*%coeficientes(formula)
+                 predicted
+               }
+               ,resid
+               = function(){
+                 residuals <<- dep - predicted
+                 residuals
+               }
+              ))
+linregRC <- function(formula, data){
+  regresion <- linreg$new(data = data, formula = formula)
+  regresion$coefficients()
+  regresion$pred()
+  regresion$resid()
+  regresion
+}
+aa <- linregRC(regres, iris)
+aa$data
+aa$pred()
+class(aa)
+print(aa)
