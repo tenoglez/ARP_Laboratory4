a9e98574bc6f82fa28c0d19c75b67856c683552e
ejarkm <ejarkm@gmail.com>
2016-09-19 12:28:04 +0200

    Implemented QRlinreg function

--- QRlinreg.R
+++ QRlinreg.R
@@ -1,7 +1,41 @@
-class(aa$indep)
-Q <- aa$indep[,1:3]/sqrt(sum(aa$indep[,1:3]^2))
-R<-t(Q)%*%aa$indep
-ff <-solve(t(R)%*%R)%*%t(R)%*%t(Q)%*%aa$dep 
-ss<-lm(regres)
-Q%*%t(Q)
-round((ss$coefficients),6) == round(ff,6)
+linreg <- setRefClass("linreg", fields = c("indep", "dep", "coefficients", "predicted", "residuals", "data", "formula"))
+linreg$methods(list(coef
+                    = function(){
+                      indep <<- model.frame(formula)
+                      dep <<- as.matrix(indep[1])
+                      colnames(dep) <<- colnames(indep)[1]
+                      indep <<- cbind(rep(1,length(indep)),as.matrix(indep[-1]))
+                      
+                      Q <- indep[,1:dim(indep)[2]]/sqrt(sum(indep[,1:dim(indep)[2]]^2))
+                      R <- t(Q)%*%indep
+                      coefficients <<- solve(t(R)%*%R)%*%t(R)%*%t(Q)%*%dep 
+                      
+                      #coefficients <<- as.vector(solve(crossprod(indep))%*%t(indep)%*%dep)
+                      names(coefficients) <<- c("(Intercept)", colnames(indep)[-1])
+                      coefficients
+                    }
+                    ,pred
+                    = function(){
+                      predicted <<- indep%*%coefficients
+                      predicted
+                    }
+                    ,resid
+                    = function(){
+                      residuals <<- dep - predicted
+                      residuals
+                    }
+)
+)
+
+print.linreg <- function(regression){
+  regression$coef
+}
+
+QRlinreg <- function(formula, data){
+  regression <- linreg$new(data = data, formula = formula)
+  regression$coef()
+  regression$pred()
+  regression$resid()
+  
+  return(regression)
+}
