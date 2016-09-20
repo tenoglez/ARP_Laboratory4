d7d46f7cf01d9380fee7682775fcd59b5457848d
ejarkm <ejarkm@gmail.com>
2016-09-20 14:25:31 +0200

    added option to plot function, to plot using standarized residuals

--- linearRegression/R/plot.linreg.R
+++ linearRegression/R/plot.linreg.R
@@ -16,18 +16,22 @@
 
 plot.linreg <- function(linreg.obj, standarized.residuals = FALSE){
   library(ggplot2)
-  #if (standarized.residuals == TRUE){
-    
-  #}
+  if (standarized.residuals == TRUE){
+    standarized <- sqrt(abs((linreg.obj$residuals - mean(linreg.obj$residuals))/sd(linreg.obj$residuals)))
+    df <- data.frame(linreg.obj$predicted, standarized)
+    ylabel.name <- "Standarized Residuals"
+  }else{
+    df <- data.frame(linreg.obj$predicted, linreg.obj$residuals)
+    ylabel.name <- "Residuals"
+  }
   
-  df <- data.frame(linreg.obj$predicted, linreg.obj$residuals)
   colnames(df) <- c("predicted", "residuals")
   
   gp <- ggplot(df,aes(x=predicted, y=residuals))
   gp <- gp + geom_point()+theme_bw()
   gp <- gp + geom_smooth(method=lm,se=FALSE)
   gp <- gp + xlab("Fitted values")
-  gp <- gp + ylab("Residuals")
+  gp <- gp + ylab(ylabel.name)
   gp <- gp + ggtitle("Residuals vs Fitted")
   gp
 }
