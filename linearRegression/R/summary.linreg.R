78f1e7081678736cb75971aa47f16412a1fc9353
ejarkm <ejarkm@gmail.com>
2016-09-21 13:10:04 +0200

    Added p-values to summary and corrected rownames

--- linearRegression/R/summary.linreg.R
+++ linearRegression/R/summary.linreg.R
@@ -18,7 +18,7 @@ summary.linreg <- function(linreg.obj){
   quants <- quantile(linreg.obj$residuals, probs = seq(0, 1, 0.25))
   std.error <- sqrt(linreg.obj$variance)
   t.val <- coefficients/sqrt(linreg.obj$variance)
-  p.val <- 2*(1-abs(t.val), linreg.obj$degrees_freedom)
+  p.val <- 2*pt(1-abs(t.val), linreg.obj$degrees_freedom)
   
   df <- data.frame(linreg.obj$coefficients, std.error, t.val, p.val)
   colnames(df) <- c("Estimate", "Std. Error", "t-value", "Pr(>|t|)" )
@@ -26,3 +26,4 @@ summary.linreg <- function(linreg.obj){
   
   list(Residuals=quants, Coefficients=df)
 }
+summary(aa)
